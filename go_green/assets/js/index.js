c864cedd12c2f65dcd05c284c3519628aa5cdd4a
ajay272191 <ajayrmavs33236@gmail.com>
2021-09-30 18:43:33 +0530

    made header responsive

--- go_green/assets/js/index.js
+++ go_green/assets/js/index.js
@@ -1,3 +1,18 @@
-// function addBorder(){
-//   console.log("hello");
+let  count = 0;
+function toggleMenu(){
+  // console.log("hello");
+  if(count == 0){
+    document.getElementsByClassName('navbar_cover')[0].style.display = "none";
+    count = count + 1;
+    console.log("hello" + count);
+  }else {
+    document.getElementsByClassName('navbar_cover')[0].style.display = "block";
+    count = count - 1;
+    console.log("hello" + count);
+  }
+}
+
+
+// document.getElementsByClassName('hamburger').addEventLisrener{
+//
 // }
